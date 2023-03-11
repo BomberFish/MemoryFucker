05de802313ed908f509a3c7008c80a740049ec92
BomberFish <87151697+BomberFish@users.noreply.github.com>
2023-03-11 16:58:04 -0500

    unfuck memory

--- MemoryFucker/ContentView.swift
+++ MemoryFucker/ContentView.swift
@@ -8,18 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var currentlyFuckingMemory = false
     var body: some View {
         VStack {
             Button(action: {
+                currentlyFuckingMemory = true
                 // memory fucking code
                 let window = UIApplication.shared.keyWindow
-                while true && window != nil {
+                while currentlyFuckingMemory && window != nil {
                     window?.snapshotView(afterScreenUpdates: false)
                 }
             }, label: {
                 Label("Fuck Memory!", systemImage: "memorychip")
-                    .padding()
+                    //.padding()
+                    .font(.largeTitle)
             })
+            if currentlyFuckingMemory {
+                Text("Fucking memory...")
+                    .font(.headline)
+                Button(action: {
+                    currentlyFuckingMemory = false
+                }, label: {
+                    Label("MAKE IT STOP!!!", systemImage: "xmark.octagon")
+                        //.padding()
+                        .font(.largeTitle)
+                })
+            }
         }
         .padding()
     }
