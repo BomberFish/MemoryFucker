7b333ef82b3aa0450f750b735a73f604402d372b
BomberFish <87151697+BomberFish@users.noreply.github.com>
2023-03-11 16:49:48 -0500

    Initial Commit

---
+++ MemoryFucker/MemoryFuckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryFuckerApp.swift
+//  MemoryFucker
+//
+//  Created by Hariz Shirazi on 2023-03-11.
+//
+
+import SwiftUI
+
+@main
+struct MemoryFuckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
